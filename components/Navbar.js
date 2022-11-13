26435e3c389a3562a2e99cb1de1f23d049df5841
CSmohaned <mohanedadel65@gmail.com>
2022-11-14 00:07:42 +0200

    add login logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,19 +3,22 @@ import Image from 'next/image'
 import AuthContext from '../stores/authContext';
 import { useContext } from 'react';
 export default function Navbar() {
-  const { user, login, logout } = useContext(AuthContext);
+  const { user, login, logout , authReady } = useContext(AuthContext);
   console.log(user);
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
+        {authReady && (
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
-          <li onClick={login} className="btn">Login/Signup</li>
-          <li onClick={logout} className="btn">Logout</li>
+            {!user && <li onClick={login} className="btn">Login/Signup</li>}
+            {user && <li>{user.email}</li>}
+            {user && <li onClick={logout} className="btn">Logout</li>}
         </ul>
+        )}
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
